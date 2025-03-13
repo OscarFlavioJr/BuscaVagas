4136c6bb33700ae749678b969412111625d33619
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2025-03-13 14:06:30 -0300

    Atualização do vd da API

--- backend/api.py
+++ backend/api.py
@@ -1,24 +1,37 @@
 from fastapi import FastAPI
-import sqlite3
+import mysql.connector
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+DB_CONFIG = {
+  "host": "localhost", 
+    "user": "ILMJ",
+    "password": "ILMJ2k25",
+    "database": "vagas",
+    "port" : "3306"
+}
+
 @app.get("/vagas")
 def get_vagas():
-    conn = sqlite3.connect("vagas.db")
+    # Conectando ao MySQL
+    conn = mysql.connector.connect(**DB_CONFIG)
     cursor = conn.cursor()
 
-    cursor.execute("SELECT titulo, link, empresa FROM vagas")
-    vagas = [{"titulo": row[0], "link": row[1], "empresa": row[2]} for row in cursor.fetchall()]
-
-    conn.close()
-    return vagas
+    try:
+        cursor.execute("SELECT titulo, link, empresa FROM vagas")
+        vagas = [{"titulo": row[0], "link": row[1], "empresa": row[2]} for row in cursor.fetchall()]
+        return vagas
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        cursor.close()
+        conn.close()
